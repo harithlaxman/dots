a17964db2df03dd0abffadb62cb2f484ea978dff
harithlaxman <harithlaxman@gmail.com>
2026-07-16 23:38:35 -0400

    added themery to choose themes from - instead of changing themes manually

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,42 @@
 return {
-    --"catppuccin/nvim",
-    --name = "catppuccin",
-    --priority = 1000,
-    --opts = {
-    --    flavour = "frappe",
-    --    transparent_background = true
-    --},
-    --config = function(_, opts)
-    --    require("catppuccin").setup(opts)
-    --    vim.cmd.colorscheme("catppuccin")
-    --end
-    "navarasu/onedark.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-        require('onedark').setup {
-            style = 'dark',
-            transparent = true,
-        }
-        require('onedark').load()
-        vim.cmd.colorscheme("onedark")
-    end
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000, -- make sure to load this before all the other start plugins
+    },
+    {
+        "zaldih/themery.nvim",
+        lazy = false,
+        config = function()
+            require("themery").setup({
+                themes = {
+                    {
+                        name = "onedark-dark",
+                        colorscheme = "onedark",
+                        before = [[
+                            require('onedark').setup {
+                                style = 'dark',
+                                transparent = true,
+                            }
+                        ]]
+                    },
+                    {
+                        name = "catppuccin-frappe",
+                        colorscheme = "catppuccin",
+                        before = [[
+                            require('catppuccin').setup {
+                                flavour = 'frappe',
+                                transparent_background = true,
+                            }
+                        ]]
+                    }
+                },
+                livePreview = true,
+            })
+        end
+    }
 }
